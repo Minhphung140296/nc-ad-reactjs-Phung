db086e528e8219be7ab2b87ac92a3c4b66a1018c
le phung <lephung@les-MacBook-Pro.local>
2020-06-25 20:32:30 +0700

    first

--- TypeScript/Day1/helloword.js
+++ TypeScript/Day1/helloword.js
@@ -1,2 +1,7 @@
-var a = "Hello word";
-console.log(a);
+var message = "Hello world";
+var age = 38;
+var isOK = true;
+var files;
+var student;
+files = ["helloword.ts", "helloword.js"];
+student = { id: 100, name: "Nodric Coder" };
--- TypeScript/Day1/helloword.ts
+++ TypeScript/Day1/helloword.ts
@@ -1,2 +1,9 @@
-let a ="Hello word"
-console.log(a);
+let message : string = "Hello world";
+const age: number = 38;
+const isOK: boolean = true;
+let files: string[];
+let student: { id: number; name: string;};
+
+files = ["helloword.ts","helloword.js"]
+student = {id:100,name:"Nodric Coder"}
+
