cc67d7129b0efe91b9fe80a2cd30693086520c2e
le phung <lephung@les-MacBook-Pro.local>
2020-06-25 20:40:22 +0700

    first

--- TypeScript/Day1/helloword.ts
+++ TypeScript/Day1/helloword.ts
@@ -1,7 +1,7 @@
 let message : string = "Hello world";
 const age: number = 38;
 const isOK: boolean = true;
-let files: string[];
+let files: string[]
 let student: { id: number; name: string;};
 
 files = ["helloword.ts","helloword.js"]
