7f975d23ae96d7b0d8c82ad3c3cd56dfdd029a2c
le phung <lephung@les-MacBook-Pro.local>
2020-06-26 16:17:57 +0700

    Create function with callback hell (3x) which use interface

--- TypeScript/HomeworkDay1/person.js
+++ TypeScript/HomeworkDay1/person.js
@@ -1,5 +1,18 @@
-var person = /** @class */ (function () {
-    function person() {
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Person = /** @class */ (function () {
+    function Person() {
         var _this = this;
         this.firstname = '';
         this.lastname = '';
@@ -7,9 +20,86 @@ var person = /** @class */ (function () {
             return "Hi " + _this.firstname + " " + _this.lastname;
         };
     }
-    return person;
+    return Person;
 }());
-var person1 = new person();
+var person1 = new Person();
 person1.firstname = 'Le';
 person1.lastname = 'Phung';
 console.log(person1.sayHi());
+var Person1 = /** @class */ (function () {
+    function Person1() {
+        var _this = this;
+        this.firstname1 = '';
+        this.lastname1 = '';
+        this.sayHi = function () {
+            return "Hi " + _this.firstname1 + " " + _this.lastname1;
+        };
+    }
+    return Person1;
+}());
+//ke thua Person1
+var Employee = /** @class */ (function (_super) {
+    __extends(Employee, _super);
+    function Employee() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Employee;
+}(Person1));
+// ke thua Employee
+var Company = /** @class */ (function (_super) {
+    __extends(Company, _super);
+    function Company() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.salary = 0;
+        return _this;
+    }
+    return Company;
+}(Employee));
+var c1 = new Company(); //Tao 1 company rong 
+c1.firstname1 = 'Le';
+c1.lastname1 = 'Phung';
+console.log(c1.sayHi()); // Hi Le Phung
+//---------------------------------------------------//
+//create function 3 callback hell (3x) which use interface
+//Cac buoc chuan bi lam 1 chiec banh burger:
+// Get ingredients (we’re gonna assume it’s a beef burger)
+// Cook the beef
+// Get burger buns
+// Put the cooked beef between the buns
+// Serve the burger
+// const makeBurger =()=>{
+//     getBeef(function(beef){
+//         //we can only cook beef after we can it 
+//         cookBeef(beef, function(cookedBeef){
+//             getBuns(function(buns){
+//                 //Put patty in Buns
+//                 putBeefBetweenBuns(buns, beef, function(burger){
+//                     //Serve the burger
+//                     nextStep(burger)
+//                 })
+//             })
+//         })
+//     })
+// }
+// // make and serve the burger
+// makeBurger(function(burger){
+//     serve(burger);
+// })
+function ThucDay(viecnaodo) {
+    viecnaodo();
+}
+function DanhRang(viecnaodo) {
+    viecnaodo();
+}
+function DiAnSang(viecnaodo) {
+    viecnaodo();
+}
+function main() {
+    ThucDay(function () {
+        DanhRang(function () {
+            DiAnSang(function () {
+                console.log("OMG!!!!");
+            });
+        });
+    });
+}
--- TypeScript/HomeworkDay1/person.ts
+++ TypeScript/HomeworkDay1/person.ts
@@ -17,3 +17,85 @@ person1.firstname = 'Le';
 person1.lastname = 'Phung';
 
 console.log(person1.sayHi());
+  //--------------------------------------------------------//
+  //create 3 class with interface which uses interface
+interface Iperson1{
+    firstname1: string
+    lastname1: string
+    sayHi: () =>string
+}
+
+class Person1 implements Iperson1{
+    firstname1= '';
+    lastname1='';
+    sayHi= () =>{
+        return `Hi ${this.firstname1} ${this.lastname1}`
+    }
+}
+interface IEmployee{
+    department: string
+    }
+    //ke thua Person1
+class Employee extends Person1 implements IEmployee{
+    department:''
+}
+interface ICompany{
+    salary: number
+}
+// ke thua Employee
+class Company extends Employee implements ICompany{
+    salary = 0
+}
+
+const c1 = new Company();//Tao 1 company rong 
+c1.firstname1 = 'Le'  
+c1.lastname1 = 'Phung'
+console.log(c1.sayHi()); // Hi Le Phung
+
+//---------------------------------------------------//
+//create function 3 callback hell (3x) which use interface
+//Cac buoc chuan bi lam 1 chiec banh burger:
+// Get ingredients (we’re gonna assume it’s a beef burger)
+// Cook the beef
+// Get burger buns
+// Put the cooked beef between the buns
+// Serve the burger
+
+// const makeBurger =()=>{
+//     getBeef(function(beef){
+//         //we can only cook beef after we can it 
+//         cookBeef(beef, function(cookedBeef){
+//             getBuns(function(buns){
+//                 //Put patty in Buns
+//                 putBeefBetweenBuns(buns, beef, function(burger){
+//                     //Serve the burger
+//                     nextStep(burger)
+//                 })
+//             })
+//         })
+//     })
+// }
+// // make and serve the burger
+// makeBurger(function(burger){
+//     serve(burger);
+// })
+
+function ThucDay(viecnaodo){
+    viecnaodo();
+}
+
+function DanhRang(viecnaodo){
+    viecnaodo();
+}
+function DiAnSang(viecnaodo){
+    viecnaodo();
+}
+function main (){
+    ThucDay(function(){
+        DanhRang(function(){
+            DiAnSang(function(){
+                console.log("OMG!!!!");
+            });
+        });
+    });
+}
